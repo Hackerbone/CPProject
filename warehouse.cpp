5d951ff3e08895843809cf36d1bd743d9ebe2f8e
Sitaraman S <sitaraman.raghav.india@gmail.com>
2019-01-14 13:09:15 +0530

    Update warehouse.cpp

--- warehouse.cpp
+++ warehouse.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 #include<stdio.h>
-#include"weight.h"
-#include"ttt.h"
-using namespace std;
-//MENU
- int main() {
-		system("clear");
-		char mchoice[10];
-#include <iostream>
-#include<stdio.h>
+#include<stdlib.h>
 #include"consumer.cpp"
-// #include"ttt.h"
+#include"weight.h"
 using namespace std;
 //MENU
  int main() {
@@ -39,35 +31,6 @@ switch (mchoice[0]) {
   default:
   cout << "Invalid input try again" << '\n';
   getchar();
-  break;
-}
-
-		}
-		while(mchoice[0] != '3');
-
-		return 0;
- }
-
-		do{
-		system("clear");
-		cout << "\n Welcome to Warehouse Operations" << endl
-		<< " Chose the number corresponding to your role: " << endl
-		<< "\n MENU" << "\n 1]Supplier \n 2]Consumer \n 3]Exit \nEnter Response: ";
-
-		cin>> mchoice;
-
-switch (mchoice[0]) {
-  case '1':
-  ttt();
-  break;
-  case '2':
-  weight();
-  break;
-  case '3':
-  mchoice[0] = '3';
-  break;
-  default:
-  cout << "Invalid input try again" << '\n';
   break;
 }
 

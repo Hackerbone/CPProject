cd70467cf336dea03ed61863fefc2edfc5ae7abb
Sitaraman S <sitaraman.raghav.india@gmail.com>
2019-08-31 23:08:59 +0530

    Some nice looking code atlast

--- new.cpp
+++ new.cpp
@@ -4,127 +4,151 @@
 #include <string.h>
 using namespace std;
 
-void hr(int x){
+void hr(int x)
+{
     cout << "+";
-    for(int i=0;i<x;i++){
+    for (int i = 0; i < x; i++)
+    {
         cout << "-";
     }
-    cout << "+\n";
+    cout << "\n";
 }
-void clear(){
+void clear()
+{
     system("clear");
 }
 
-class supplier{
+class supplier
+{
     //About Supplier
-    string supplier_name;
-    string supplier_password;
-    string supplier_sname;
-    string supplier_username;
+    char supplier_name[100];
+    char supplier_password[20];
+    char supplier_sname[100];
+    char supplier_username[100];
     //Product
-    string product_name;
+    char product_no[20];
+    char product_name[100];
     long int product_cost;
-    string product_color;
-    string product_symbol;
-    public:
-        void supplier_product_input()
-        {   
-            cout << "|| Welcome To Product Selling || \nEnter Prouct name\n";
-            cin >> product_name;
-            cout  << "Enter Product Cost\n";
-            cin >> product_cost;
-            cout << "Enter Product Color\n";
-            cin >> product_color;
-            cout << "Enter Product Color Symbol in [color] form\n";
-            cin >> product_symbol;
-        }
-        void product_file_input()
-        {   
-            fstream product_file;
-            supplier s;
-            s.supplier_product_input();
-            product_file.open("products.txt", ios::out | ios::app | ios::binary);
-            product_file.write((char*)&s, sizeof(s));
-            product_file.close();
-        }
-        void view(){
-            ifstream product_file;
-            supplier s;
-            product_file.open("products.txt");
-            int i = 1;
-            cout << "| Sno |   Product    |   Cost   |   Color   |" << endl;
-            do
-            {   
-                product_file.read((char*)&s, sizeof(s));
-                cout <<"   " << i <<"     "<< s.product_name << "       "<< s.product_cost << "$        " << s.product_color << "\n";
-                hr(43);
-                i++;
-            } 
-            while(product_file.read((char*)&s, sizeof(s)));
-            product_file.close();
-            cin.ignore();
-            cin.get();
-        }
-        void supplier_signup()
-        {
-            clear();
-            cout << "Enter your Details\n";
-            cout << "Enter your first name: ";
-            cin >> supplier_name;
-            cout << endl;
-            cout << "\nEnter your last name: ";
-            cin >> supplier_sname;
-            cout << endl;
-            cout << "\nEnter your Username: ";
-            cin >> supplier_username;
-            cout << endl;
-            cout << "\nEnter your password: ";
-            cin >> supplier_password;
-            cout << endl;
-        }
-        void supplier_login()
-        {
-            int present;
-            supplier x;
-            cout << "Enter your Username: ";
-            cin >> x.supplier_username;
+    char product_color[100];
 
-            cout << "\nEnter your password: ";
-            cin >> x.supplier_password;
-            cout << endl;
-            supplier_login_check(x, present);
-            if(present == 1){
-                product_file_input();
-            }
-            else{
-                cout << "Record Not Found! Try signing up";
-            }
+public:
+    void supplier_product_input()
+    {
+        cout << "|| Welcome To Product Selling || \nEnter product name\n";
+        cin >> product_name;
+        cout << "Enter Serial No\n";
+        cin >> product_no;
+        cout << "Enter Product Cost\n";
+        cin >> product_cost;
+        cout << "Enter Product Color\n";
+        cin >> product_color;
+        cin.get();
+        cin.ignore();
+    }
+    void product_file_input()
+    {
+        fstream product_file;
+        supplier s;
+        s.supplier_product_input();
+        product_file.open("products.txt", ios::out | ios::app | ios::binary);
+        product_file.write((char *)&s, sizeof(s));
+        product_file.close();
+    }
+    void view_product()
+    {
+        ifstream product_file;
+        supplier s;
+        product_file.open("products.txt", ios::in | ios::binary);
+        int i = 1;
+        cout << "| Sno |   Product    |   Cost   |   Color   |" << endl;
+        do
+        {
+            product_file.read((char *)&s, sizeof(s));
+            cout << "   " << i << "     " << s.product_name << "       " << s.product_cost << "$        " << s.product_color << "\n";
+            hr(43);
+            i++;
+        } while (product_file.read((char *)&s, sizeof(s)));
+        product_file.close();
+        cin.ignore();
+        cin.get();
+    }
+
+    void view()
+    {
+        ifstream product_file;
+        supplier s;
+        product_file.open("supplier.txt", ios::in | ios::binary);
+        int i = 1;
+        do
+        {
+            product_file.read((char *)&s, sizeof(s));
+            cout << "   " << i << "     " << s.supplier_username << "       " << s.supplier_password << "        "
+                 << "\n";
+            hr(43);
+            i++;
+        } while (product_file.read((char *)&s, sizeof(s)));
+        product_file.close();
+        cin.ignore();
+        cin.get();
+    }
+
+    void supplier_signup()
+    {
+        clear();
+        cout << "Enter your Details\n";
+        cout << "Enter your first name: ";
+        cin >> supplier_name;
+        cout << endl;
+        cout << "\nEnter your last name: ";
+        cin >> supplier_sname;
+        cout << endl;
+        cout << "\nEnter your Username: ";
+        cin >> supplier_username;
+        cout << endl;
+        cout << "\nEnter your password: ";
+        cin >> supplier_password;
+        cout << endl;
+    }
+    void supplier_login()
+    {
+        int present;
+        supplier x;
+        cout << "Enter your Username: ";
+        cin >> x.supplier_username;
+        cout << "\nEnter your password: ";
+        cin >> x.supplier_password;
+        cout << endl;
+        present = 0;
+        ifstream supply_file;
+        supplier s;
+        supply_file.open("supplier.txt", ios::in | ios::binary);
+        //Looping and reading all file contents
+        while (supply_file.read((char *)&s, sizeof(s)))
+        {
+
+            if ((strcmp(s.supplier_username, x.supplier_username) == 0) && (strcmp(s.supplier_password, x.supplier_password) == 0))
+                present = 1;
         }
-       void supplier_login_check(supplier x, int &present){
-           //Checking if record is present in file;
-           present = 0;
-           ifstream supply_file;
-           supplier s;
-           supply_file.open("supplier.txt", ios::in | ios::binary);
-           //Looping and reading all file contents
-           while (!supply_file.eof())
-           {
-               supply_file.read((char *)&s, sizeof(s));
-               if (s.supplier_username == x.supplier_username && s.supplier_password == x.supplier_password)
-               {
-                   present = 1;
-               }
-           }
-           supply_file.close();
-       }        
-     
+        supply_file.close();
+        if (present == 1)
+        {
+            product_file_input();
+        }
+        else
+        {
+            cout << "Record Not Found! Try signing up";
+            cin.get();
+            cin.ignore();
+        }
+    }
+
     void supplier_file_input()
     {
         fstream supply_file;
         supplier s;
         s.supplier_signup();
-        supply_file.open("supplier.txt", ios::app);
-        supply_file.write((char*)&s, sizeof(s));
+        supply_file.open("supplier.txt", ios::out | ios::app | ios::binary);
+        supply_file.write((char *)&s, sizeof(s));
         supply_file.close();
     }
 
@@ -132,7 +156,8 @@ class supplier{
     {
         char x;
         do
-        {   system("clear");
+        {
+            system("clear");
             cout << "|| Welcome To Supplier Area ||\n"
                  << "[1]Login\n[2]Signup \n([0] to exit)\n > ";
             cin >> x;
@@ -152,6 +177,174 @@ class supplier{
                 view();
             }
 
+            else if (x == 'y')
+            {
+                clear();
+                view_product();
+            }
+
+            else
+            {
+                cout << "Enter valid input";
+            }
+
+        } while (x != '0');
+    }
+};
+
+class customer
+{
+    //About customer
+    char customer_name[100];
+    char customer_password[20];
+    char customer_sname[100];
+    char customer_username[100];
+    char customer_address[100];
+    long int customer_mobile;
+
+    //Product
+    long int product_no;
+    char product_name[100];
+    long int product_cost;
+    char product_color[100];
+
+public:
+
+    void view_product()
+    {
+        ifstream product_file;
+        customer s;
+        product_file.open("products.txt", ios::in | ios::binary);
+        int i = 1;
+        cout << "| Sno |   Product    |   Cost   |   Color   |" << endl;
+        do
+        {
+            product_file.read((char *)&s, sizeof(s));
+            cout << "   " << i << "     " << s.product_name << "       " << s.product_cost << "$        " << s.product_color << "\n";
+            hr(43);
+            i++;
+        } while (product_file.read((char *)&s, sizeof(s)));
+        product_file.close();
+        cin.ignore();
+        cin.get();
+    }
+
+    void view()
+    {
+        ifstream product_file;
+        customer s;
+        product_file.open("customer.txt", ios::in | ios::binary);
+        int i = 1;
+        do
+        {
+            product_file.read((char *)&s, sizeof(s));
+            cout << "   " << i << "     " << s.customer_username << "       " << s.customer_password << "        "
+                 << "\n";
+            hr(43);
+            i++;
+        } while (product_file.read((char *)&s, sizeof(s)));
+        product_file.close();
+        cin.ignore();
+        cin.get();
+    }
+
+    void customer_signup()
+    {
+        clear();
+        cout << "Enter your Details\n";
+        cout << "Enter your first name: ";
+        cin >> customer_name;
+        cout << endl;
+        cout << "\nEnter your last name: ";
+        cin >> customer_sname;
+        cout << endl;
+        cout << "\nEnter your Username: ";
+        cin >> customer_username;
+        cout << endl;
+        cout << "\nEnter your password: ";
+        cin >> customer_password;
+        cout << endl;
+        cout << "\nEnter Shipping Address: ";
+        cin >> customer_address;
+        cout << endl;
+        cout << "\nEnter Mobile Number: ";
+        cin >> customer_mobile;
+        cout << endl;
+    }
+    void customer_login()
+    {
+        int present;
+        customer x;
+        cout << "Enter your Username: ";
+        cin >> x.customer_username;
+        cout << "\nEnter your password: ";
+        cin >> x.customer_password;
+        cout << endl;
+        present = 0;
+        ifstream supply_file;
+        customer s;
+        supply_file.open("customer.txt", ios::in | ios::binary);
+        //Looping and reading all file contents
+        while (supply_file.read((char *)&s, sizeof(s)))
+        {
+
+            if ((strcmp(s.customer_username, x.customer_username) == 0) && (strcmp(s.customer_password, x.customer_password) == 0))
+                present = 1;
+        }
+        supply_file.close();
+        if (present == 1)
+        {
+            // marketplace_menu();
+            cout << "Successfully Logged in!\n";
+        }
+        else
+        {
+            cout << "Record Not Found! Try signing up";
+            cin.get();
+            cin.ignore();
+        }
+    }
+
+    void customer_file_input()
+    {
+        fstream supply_file;
+        customer s;
+        s.customer_signup();
+        supply_file.open("customer.txt", ios::out | ios::app | ios::binary);
+        supply_file.write((char *)&s, sizeof(s));
+        supply_file.close();
+    }
+
+    void customer_menu()
+    {
+        char x;
+        do
+        {
+            system("clear");
+            cout << "|| Welcome To Customer Menu||\n"
+                 << "[1]Login\n[2]Signup \n([0] to exit)\n > ";
+            cin >> x;
+            if (x == '1')
+            {
+                clear();
+                customer_login();
+            }
+            else if (x == '2')
+            {
+                clear();
+                customer_file_input();
+            }
+            else if (x == 'x')
+            {
+                clear();
+                view();
+            }
+            else if (x == 'y')
+            {
+                clear();
+                view_product();
+            }
+
             else if (x == '0')
             {
                 break;
@@ -166,11 +359,12 @@ class supplier{
     }
 };
 
-
-void menu(){
+void menu()
+{
     char a;
     //Do menu starts
-    do{
+    do
+    {
         system("clear");
         cout << "|| Welcome to Estore where you can buy or supply Electronic Products! ||\n"
              << "[1] Supplier \n[2] Customer \n\n([0] to Exit)\n> ";
@@ -180,31 +374,30 @@ void menu(){
             clear();
             supplier supply_session;
             supply_session.supplier_menu();
+        }
+        else if (a == '2')
+        {
+            clear();
+            customer customer_session;
+            customer_session.customer_menu();
+        }
+        else if (a == '0')
+        {
+            break;
+        }
 
-    }
-    else if (a == '2')
-    {
-        clear();
-        supplier x;
-        x.view();
-    }
-    else if (a == '0')
-    {
-        break;
-    }
+        else
+        {
+            cout << "Tri dont cri";
+        }
 
-    else{
-        cout << "Tri dont cri";
-    }
-
-    }while(a!='0');
-//Menu 1 ends
-
-
+    } while (a != '0');
+    //Menu 1 ends
 }
 
-    int main(){
-        system("clear");
-        menu();
-        return 0;
-    }
+int main()
+{
+    system("clear");
+    menu();
+    return 0;
+}
